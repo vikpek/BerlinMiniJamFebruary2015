0d254ca1ef0f2c0b18cd578688b065540b733929
Viktor Pekar <vikpek@gmail.com>
2015-02-28 16:55:19 +0100

    asdf

--- ReturnOfSpock/Assets/RandomBlackHoleGenerator.cs
+++ ReturnOfSpock/Assets/RandomBlackHoleGenerator.cs
@@ -5,18 +5,31 @@ using System.Collections.Generic;
 public class RandomBlackHoleGenerator : MonoBehaviour {
 
 	GameObject[] blackHoles;
-//	Dictionary 
+	List<Integer> froms;
+	List<Integer> tos;
 
 	void Awake()
 	{
 		blackHoles = GameObject.FindGameObjectsWithTag("BlackHole");
+		//initial fill
+		for(int i = 0; blackHoles.Length; i++)
+		{
+			froms.Add(i);
+			tos.Add(i);
+		}
 	}
 
 
 	void Start () {
 		foreach(GameObject blackHole in blackHoles)
 		{
-			
+
+		for(int i = 0; blackHoles.Length; i++)
+		{
+//			blackHoles[i].GetComponent<BlackHoleController>().
+		}
+//			blackHole.GetComponent<BlackHoleController>().blackHoleID
+//			blackHole.GetComponent<BlackHoleController>().previousBlackHole = froms[
 		}
 	}
 	
